0ec507dd1827a268a0df385928d2b4a6e857988b
Fredy Nurman Saleh <email@fredyns.net>
2016-10-13 08:33:16 +0700

    configs

--- config/params.php
+++ config/params.php
@@ -1,14 +1,7 @@
 <?php
 return [
-    'adminEmail' => 'email@fredyns.net',
-    'admins'     => ['admin', 'fredy.ns'],
+    'adminEmail'     => 'email@fredyns.net',
     'language'   => 'id-ID',
-    'timeZone'   => 'Asia/Jakarta',
-    'mailer'     => [
-        'class'            => 'yii\swiftmailer\Mailer',
-        // send all mails to a file by default. You have to set
-        // 'useFileTransport' to false and configure a transport
-        // for the mailer to send real emails.
-        'useFileTransport' => true,
-    ],
+    'timeZone'       => 'Asia/Jakarta',
+    'icon-framework' => 'fa',
 ];
--- config/web.php
+++ config/web.php
@@ -1,5 +1,6 @@
 <?php
 $params = require(__DIR__.'/params.php');
+$secret = require(__DIR__.'/secret.php');
 
 $config = [
     'id'            => 'yii2-playground',
@@ -10,8 +11,16 @@ $config = [
     'bootstrap'     => ['log'],
     'components'    => [
         'request'      => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'GkjkF$AbfLKASNFf',
+            'cookieValidationKey' => $secret['cookieValidationKey'],
+        ],
+        'response'     => [
+            'formatters' => [
+                \yii\web\Response::FORMAT_JSON => [
+                    'class'         => 'yii\web\JsonResponseFormatter',
+                    'prettyPrint'   => TRUE,
+                    'encodeOptions' => JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE,
+                ],
+            ],
         ],
         'session'      => [
             'class'        => 'yii\web\DbSession',
@@ -30,7 +39,7 @@ $config = [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'mailer'       => $params['mailer'],
+        'mailer'       => $secret['mailer'],
         'log'          => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets'    => [
@@ -57,17 +66,20 @@ $config = [
         'user'             => [
             'class'                  => 'dektrium\user\Module',
             'enableUnconfirmedLogin' => TRUE,
-            'admins'                 => $params['admins'],
+            'admins'                 => $secret['admins'],
             'modelMap'               => [
                 'User'    => 'app\models\User',
                 'Profile' => 'app\models\Profile',
             ],
             'controllerMap'          => require(__DIR__.'/module/user-controllerMap.php'),
         ],
+        'gridview'         => [
+            'class' => '\kartik\grid\Module'
+        ],
         'daerah-indonesia' => [
             'class'         => 'fredyns\daerahIndonesia\Module',
             'controllerMap' => require(__DIR__.'/module/daerahIndonesia-controllerMap.php'),
-        ]
+        ],
     ],
     'controllerMap' => [
         'file' => 'mdm\\upload\\FileController', // use to show or download file
@@ -85,7 +97,15 @@ if (YII_ENV_DEV)
 
     $config['bootstrap'][]    = 'gii';
     $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
+        'class'      => 'yii\gii\Module',
+        'generators' => [
+            'giiant-model' => [
+                'class' => 'fredyns\giiantTemplate\model\Generator',
+            ],
+            'giiant-crud'  => [
+                'class' => 'fredyns\giiantTemplate\crud\Generator',
+            ],
+        ],
     ];
 }
 
